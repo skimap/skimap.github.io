5624df8b4e96d4d657810765b34bd1c348ddb5c1
Istvan Zsely <zselyigy@gmail.com>
2024-02-09 19:09:48 +0100

    better measure in identifications: track lenght compared to that of the reference

--- identify_tracks.py
+++ identify_tracks.py
@@ -1,4 +1,6 @@
 # identfies the corresponding ski slopes and sorts the gpx files according to them
+# just one gps track and gps segment in a file!
+
 import json
 import os
 import shutil
@@ -36,6 +38,17 @@ for filename in os.listdir(track_directory):
         gpx = gpxpy.parse(gpx_file)
         gpx_file.close()
 
+        for gpx_track in gpx.tracks:
+            # iterate over the segments in the track
+            for gpx_segment in gpx_track.segments:
+                # convert the gpx segment to a numpy array of coordinates
+                gpx_points = np.array([(p.latitude, p.longitude) for p in gpx_segment.points])
+
+        length_gpx_track = gpxpy.geo.haversine_distance(gpx_points[0][0],
+                                                        gpx_points[0][1],
+                                                        gpx_points[-1][0],
+                                                        gpx_points[-1][1])
+
         smallest_avg_distance = np.inf
         closest_ski_area = None
         closest_ski_track = None
@@ -43,24 +56,24 @@ for filename in os.listdir(track_directory):
         for ski_area in slope_ref_points['items']: # loop over the items list
             for track in ski_area['tracks']: # loop over the tracks list
                 #print(ski_area['name'], track['trackname']) # print the name and trackname of each ski area and track
-                # iterate over the tracks in the gpx object
-                for gpx_track in gpx.tracks:
-                    # iterate over the segments in the track
-                    for gpx_segment in gpx_track.segments:
-                        # convert the list of points to a numpy array of coordinates
-                        ref_points = np.array([(p['lat'], p['lon']) for p in track['points']])
-                        # convert the gpx segment to a numpy array of coordinates
-                        gpx_points = np.array([(p.latitude, p.longitude) for p in gpx_segment.points])
-                        # calculate the pairwise distances between the gpx points and the reference points
-                        distances = scipy.spatial.distance.cdist(gpx_points, ref_points)
-                        # calculate the minimal values for each reference point
-                        min_values = np.min(distances, axis=0)
-                        avg_distance = np.mean(min_values)                       
-                        #print(f'{ski_area["name"]}, {track["trackname"]}: {avg_distance}')
-                        if avg_distance < smallest_avg_distance:
-                            smallest_avg_distance = avg_distance
-                            closest_ski_area = ski_area["name"]
-                            closest_ski_track = track["trackname"]
+                # convert the list of points to a numpy array of coordinates
+                ref_points = np.array([(p['lat'], p['lon']) for p in track['points']])
+                # calculate the pairwise distances between the gpx points and the reference points
+                distances = scipy.spatial.distance.cdist(gpx_points, ref_points)
+                # calculate the minimal values for each reference point
+                min_values = np.min(distances, axis=0)
+                avg_distance = np.mean(min_values)
+                # find the length of the slopes
+                length_track = gpxpy.geo.haversine_distance(ref_points[0][0],
+                                                ref_points[0][1],
+                                                ref_points[-1][0],
+                                                ref_points[-1][1])
+                rel_length_diff = length_track / length_gpx_track
+                print(f'{ski_area["name"]}, {track["trackname"]}, {avg_distance}, {rel_length_diff}')
+                if (avg_distance < smallest_avg_distance) and (abs(rel_length_diff - 1) < 0.1):
+                    smallest_avg_distance = avg_distance
+                    closest_ski_area = ski_area["name"]
+                    closest_ski_track = track["trackname"]
                             
         # print the closest ski area and track                       
         print(f'{filename}: the closest ski area and track is {closest_ski_area}, {closest_ski_track} with avg distance {smallest_avg_distance}.')

a38285e46b0468a1b252c9f8b4bb42205e9fb000
Istvan Zsely <zselyigy@gmail.com>
2024-02-13 16:34:01 +0100

    merge tracks by collected descent rates

--- dense_ref_points_and_merge_tracks.py
+++ dense_ref_points_and_merge_tracks.py
@@ -24,6 +24,7 @@ track_directory = "tracks/identification/identified/Síaréna Vibe Park 20240210
 with open("json/slopes/Epleny_slopes.json", 'r', encoding='utf-8') as file:
     runs = json.load(file)
 
+# collect the coordinates and elevation data from the reference slope file
 for item in runs["items"]:
     for track in item["tracks"]:
         if track["trackname"] == 'A1':
@@ -35,14 +36,15 @@ for item in runs["items"]:
                 lon.append(point["lon"])
                 ele.append(point["ele"])   # elevation data will be collected from gpx files
 
-
+# make the slope coordinates more dense (no more than max_distance_in_m meters is allowed)
+max_distance_in_m = 5
 dtot  = 0
 newlat = []
 newlon = []
 newele = []
 for i in range(len(lat)-1):
     d = gpxpy.geo.haversine_distance(lat[i], lon[i], lat[i+1], lon[i+1])
-    dnum = int(d/5)
+    dnum = int(d/max_distance_in_m)
     newlat.append(lat[i])
     newlon.append(lon[i])
     newele.append(ele[i])
@@ -51,8 +53,8 @@ for i in range(len(lat)-1):
         newlon.append(lon[i]+j*(lon[i+1]-lon[i])/(dnum+1))
         newele.append(ele[i]+j*(ele[i+1]-ele[i])/(dnum+1))
 
-# this will collect the elevation data from the gpx files
-elevation_bin = [[] for _ in range(len(newlat))]
+# this collects the descent rate data from the gpx data
+rate_bin = [[] for _ in range(len(newlat))]
 
 # joe is a progress level variable iterates through all files in working directory
 joe=1
@@ -64,7 +66,6 @@ for filename in os.listdir(track_directory):
         gpx_file = open(os.path.join(track_directory, filename), 'r')
         gpx = gpxpy.parse(gpx_file)
 
-        keep_track = True
         for track in gpx.tracks:
             for segment in track.segments:
                 for j, point in enumerate(segment.points):
@@ -76,30 +77,34 @@ for filename in os.listdir(track_directory):
                             mind = d
                             mind_index = i
 
+                    # calculate and store the descent rates
                     if j == 0:
-                        if abs(newele[mind_index] - point.elevation) > 10:
-                            keep_track = False
-                    if keep_track:
-                        elevation_bin[mind_index].append(point.elevation)
+                        descent_rate = 0
+                    else:
+                        distance = gpxpy.geo.haversine_distance(segment.points[j - 1].latitude, segment.points[j - 1].longitude,
+                                segment.points[j].latitude, segment.points[j].longitude)
+                        elevation_gain = segment.points[j].elevation - segment.points[j-1].elevation
+                        if distance != 0:
+                            descent_rate = elevation_gain / distance
+                        else: 
+                            descent_rates=0
+                    rate_bin[mind_index].append(descent_rate)
         
         joe += 1
 
-# avg_elevation = []
-# with open("elevations.txt", "w", encoding="utf-8") as file:
-#     for i in range(len(elevation_bin)):
-#         if len(elevation_bin[i]) >0:
-#             avg_elevation.append(np.mean(elevation_bin[i]))
-#             print(avg_elevation[-1], file = file)
-
+# discard the densed reference points which had no descent rates
 filtered_lat = []
 filtered_lon = []
 filtered_ele = []
-for i in range(len(elevation_bin)):
-    if len(elevation_bin[i]) >0:
+filtered_rate = []
+for i in range(len(rate_bin)):
+    if len(rate_bin[i]) >0:
         filtered_lat.append(newlat[i])
         filtered_lon.append(newlon[i])
-        filtered_ele.append(np.mean(elevation_bin[i]))
+        filtered_ele.append(newele[i])
+        filtered_rate.append(np.mean(rate_bin[i]))
 
+# Write the results to a new GPX file
 # Create a new GPX object
 gpx = gpxpy.gpx.GPX()
 
@@ -112,8 +117,8 @@ gpx_segment = gpxpy.gpx.GPXTrackSegment()
 gpx_track.segments.append(gpx_segment)
 
 # Add points to the segment
-for lat, lon, ele in zip(filtered_lat, filtered_lon, filtered_ele):
-    gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat, lon, elevation=ele))
+for lat, lon, ele, rate in zip(filtered_lat, filtered_lon, filtered_ele, filtered_rate):
+    gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat, lon, elevation=ele, comment=str(rate)))
 
 # Write the GPX data to a file
 filename = 'merged.gpx'
